023f99d5ef3ee943c5bd4399cca5a4837def977b
Brandon Knight <knightbrandon1984@gmail.com>
2023-08-01 15:54:12 -0500

    added a one type of styled navbar

--- src/components/Navbar/index.js
+++ src/components/Navbar/index.js
@@ -1,10 +1,42 @@
 import React from "react";
+import { Nav, NavLink, NavMenu, NavBtn, NavBtnLink } from "./NavbarElement.js";
+
+// import {
+//   Nav,
+//   NavLink,
+//   Bars,
+//   NavMenu,
+//   NavBtn,
+//   NavBtnLink,
+// } from "./NavbarElement.js";
 
 const Navbar = () => {
   return (
-    <div>
-      <h1>Navbar</h1>
-    </div>
+    <>
+      <Nav>
+        <NavLink to="/">
+          <h1>Logo</h1>
+        </NavLink>
+
+        <NavMenu>
+          <NavLink to="/about" activeStyle>
+            About
+          </NavLink>
+          <NavLink to="/services" activeStyle>
+            Services
+          </NavLink>
+          <NavLink to="/contact-us" activeStyle>
+            Contact Us
+          </NavLink>
+          <NavLink to="/sign-up" activeStyle>
+            Sign Up
+          </NavLink>
+        </NavMenu>
+        <NavBtn>
+          <NavBtnLink to="/signin">Sign In</NavBtnLink>
+        </NavBtn>
+      </Nav>
+    </>
   );
 };
 
